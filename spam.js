ae7cb250611b57ddf9891a12565e4e6fbfa3caf6
Lukas Appelhans <l.appelhans@gmx.de>
2017-01-31 12:07:41 +0100

    Cleanup bounds a little

--- spam.js
+++ spam.js
@@ -56,14 +56,10 @@
 
     function maxBounds(one, two) {
         var bounds = two
-        if (one[0][0] < two[0][0])
-            bounds[0][0] = one[0][0]
-        if (one[0][1] < two[0][1])
-            bounds[0][1] = one[0][1]
-        if (one[1][0] > two[1][0])
-            bounds[1][0] = one[1][0]
-        if (one[1][1] > two[1][1])
-            bounds[1][1] = one[1][1]
+        bounds[0][0] = Math.min(one[0][0], two[0][0])
+        bounds[0][1] = Math.min(one[0][1], two[0][1])
+        bounds[1][0] = Math.max(one[1][0], two[1][0])
+        bounds[1][1] = Math.max(one[1][1], two[1][1])
         return bounds
     }
 
