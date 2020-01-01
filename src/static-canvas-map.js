ec54fd4e1d03724445b4c8e3f8236d7d4e38dd0a
Martín González <m@martingonzalez.net>
2020-01-01 16:29:18 +0100

    Declare constructor

--- src/static-canvas-map.js
+++ src/static-canvas-map.js
@@ -1,6 +1,10 @@
 import { CanvasMap } from "./canvas-map";
 
 export default class StaticCanvasMap {
+  constructor(parameters) {
+    this.map = new CanvasMap(parameters);
+  }
+
   init() {
     this.map.init();
   }
